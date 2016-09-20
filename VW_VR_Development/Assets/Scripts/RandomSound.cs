5070ccad6379727c0e9a177151fc0db4f2e53ce0
Ariel Santibanez <ariel.santibanez@futuregames.se>
2016-09-20 12:40:42 +0200

    Merged assets folder with folder containing new sounds/sound work.

---
+++ VW_VR_Development/Assets/Scripts/RandomSound.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSound : MonoBehaviour {
+
+    public AudioClip[] Clips;
+    public float MinSeconds;
+    public float MaxSeconds;
+    public AudioSource source;
+	private float timecount;
+	private float starttime;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine( SoundWaiter() );
+	}
+
+    IEnumerator SoundWaiter() {
+		starttime = Time.time;
+        yield return new WaitForSeconds( Random.Range( MinSeconds, MaxSeconds ) );
+        source.clip = Clips[Random.Range( 0, Clips.Length )];
+        source.Play();
+        timecount = Time.time - starttime;
+        print(timecount);
+        print(source.clip);
+        StartCoroutine( SoundWaiter() );
+    }
+
+	// Update is called once per frame
+	void Update () {
+        // yield return new WaitForSeconds( 10 );
+
+	}
+}
